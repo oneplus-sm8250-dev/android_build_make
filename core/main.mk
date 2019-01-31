5e8ce088bb24f29c2c9f6dc8c065a30dfca9941f
shoudil <stephenli@codeaurora.org>
2019-01-31 16:39:38 +0800

    QCV: add flag to indicate pure AOSP or not
    Set flag TARGET_FWK_SUPPORTS_FULL_VALUEADDS as true
    to indicate vendor modified framework. Also initialize
    the flag based property value.
    
    Change-Id: If18e637287a9f53fab2caf6c8b1691311c04b7a3
    CRs-Fixed: 2379664

--- core/main.mk
+++ core/main.mk
@@ -467,6 +467,25 @@ BUILD_WITHOUT_PV := true
 
 ADDITIONAL_SYSTEM_PROPERTIES += net.bt.name=Android
 
+# QCV: initialize property - used to detect framework type
+ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS), true)
+  ADDITIONAL_SYSTEM_PROPERTIES += \
+        ro.vendor.qti.va_aosp.support=1
+
+  ADDITIONAL_ODM_PROPERTIES += \
+        ro.vendor.qti.va_odm.support=1
+
+  $(warning "Compile using modified AOSP tree supporting full vendor value-adds")
+else
+  ADDITIONAL_SYSTEM_PROPERTIES += \
+        ro.vendor.qti.va_aosp.support=0
+
+  ADDITIONAL_ODM_PROPERTIES += \
+        ro.vendor.qti.va_odm.support=0
+
+  $(warning "Compile using pure AOSP tree")
+endif
+
 # Include vendor build hook
 -include vendor/proton/build/core/main.mk
 
